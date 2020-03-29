a951e5e93940fa822360eb7197065984b600bfab
mosuch <mariaosuch96@gmail.com>
2020-03-29 10:58:00 +0200

    napisy1.py

--- napisy1.py
+++ napisy1.py
@@ -1,4 +1,7 @@
-liczba= (input("Podaj liczbę: "))
-print(float(liczba)>10)
-print(float(liczba)<=15)
-print(float(liczba)%2==0)
+krawedz_a = float(input("Podaj długość krawędzi a: "))
+krawedz_b = float(input("Podaj długość krawędzi b: "))
+krawedz_c = float(input("Podaj długość krawędzi c: "))
+
+obj = krawedz_a * krawedz_b * krawedz_c
+print(f"Objętość opakowania jest równa {obj} cm^3")
+print(obj >1000)
